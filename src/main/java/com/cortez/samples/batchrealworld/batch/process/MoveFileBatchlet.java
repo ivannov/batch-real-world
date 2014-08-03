855d3f6f1fffa83faa401ee1e7ed524118700437
Roberto Cortez <radcortez@yahoo.com>
2014-08-03 22:08:50 +0100

    Generic move file batchlet.

--- src/main/java/com/cortez/samples/batchrealworld/batch/process/MoveFileBatchlet.java
+++ src/main/java/com/cortez/samples/batchrealworld/batch/process/MoveFileBatchlet.java
@@ -1,6 +1,11 @@
 package com.cortez.samples.batchrealworld.batch.process;
 
+import com.cortez.samples.batchrealworld.entity.FolderType;
+
+import javax.batch.api.BatchProperty;
 import javax.batch.api.Batchlet;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.File;
 
@@ -13,12 +18,19 @@ import static org.apache.commons.io.FileUtils.moveFileToDirectory;
  */
 @Named
 public class MoveFileBatchlet extends AbstractFileProcess implements Batchlet {
+    @Inject
+    @BatchProperty(name = "from")
+    private String from;
+    @Inject
+    @BatchProperty(name = "to")
+    private String to;
+
     @Override
     public String process() throws Exception {
         System.out.println("MoveFilesBatchlet.process");
 
-        File file = getFileToProcess(FI);
-        File destinationFolder = getFolder(FI_TMP);
+        File file = getFileToProcess(FolderType.valueOf(from));
+        File destinationFolder = getFolder(FolderType.valueOf(to));
 
         System.out.println("Moving file " + file + " to " + destinationFolder);
         moveFileToDirectory(file, destinationFolder, false);
